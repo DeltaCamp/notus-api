1e3041dbffd5f28ba3a0655387689b5718c15c39
Charles Bergeron <chuck.bergeron@gmail.com>
2019-03-17 13:41:53 -0700

    * continuing to work through the DB CRUD for dapps

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # notus-api
 The Notus API Server in Node/Express.
+
+```sh
+curl http://localhost:3000/dapps -X POST -H 'Authorization: Bearer dG9rOjIwMDU4MmRkXzMzZDFfNDkyZl85NDViX2Q0ZjZhNDc2OWM0ZDoxOjA=' -H 'Accept: application/json' -H 'Content-Type: application/json' -d '
+{
+  "email": "wash@serenity.io",
+  "dappName": "Hoban Dudes"
+}'
+```
+
+
+```sql
+psql postgres -U chuckbergeron;
+
+CREATE DATABASE notusapi;
+
+GRANT ALL PRIVILEGES ON DATABASE notusapi TO chuckbergeron;
+```
+
+
+replace creds with env vars
--- src/dapps/dapp.controller.ts
+++ src/dapps/dapp.controller.ts
@@ -1,16 +1,56 @@
-import { Controller, Post, Get } from '@nestjs/common';
-import { Connection } from 'typeorm';
-// import { CreateDappDto } from './create-dapp.dto';
+import { Controller, Get, Post, Put, Delete, Request, Response, Body, Param, HttpStatus } from '@nestjs/common';
+import { DappService } from "./dapp.service";
+import { Dapp } from "../entity/dapp.entity";
 
 @Controller('dapps')
 export class DappController {
-  @Post()
-  async create() {
-  // async create(@Body() createDappDto: CreateDappDto) {
-    let status
 
-    status = 'ok'
+  constructor(
+    private readonly dappService: DappService
+  ) { }
 
-    return status
+  @Get()
+  findAll(): Promise<Dapp[]> {
+    return this.dappService.findAll();
   }
+
+  // @Get()
+  // public async getAllDapp(@Response() res) {
+  //   const dapps = await this.dappService.findAll();
+  //   res.status(HttpStatus.OK).json(dapps);
+  // }
+
+  // @Get('/:id')
+  // public async getDapp(@Response() res,
+  //   @Param('id') id) {
+  //   const dapp = await this.dappService.getDapp(id);
+  //   res.status(HttpStatus.OK).json(dapp);
+  // }
+
+  @Post()
+  public async createDapp(
+    @Response() res,
+    @Body('dappName') dappName,
+    @Body('email') email
+  ) {
+    const result = await this.dappService.createDapp(dappName, email);
+    res.status(HttpStatus.CREATED).json(result);
+  }
+
+  // @Put('/:id')
+  // public async updateDapp(@Response() res,
+  //   @Param('id') id,
+  //   @Body('dappName') dappName,
+  //   @Body('email') email
+  // ) {
+  //   const result = await this.dappService.updateDapp(id, dappName, email);
+  //   res.status(HttpStatus.ACCEPTED).json(result);
+  // }
+
+  // @Delete('/:id')
+  // public async deleteDapp(@Response() res,
+  //   @Param('id') id) {
+  //   const result = await this.dappService.deleteDapp(id);
+  //   res.status(HttpStatus.ACCEPTED).json(result);
+  // }
 }
