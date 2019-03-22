c6b1f7d6b41978b616814f598b7cb3d1fe0db09c
Brendan Asselstine <mail.asselstine@gmail.com>
2019-03-22 14:28:04 -0700

    Added DappUserService

--- src/dapp_users/DappUserService.ts
+++ src/dapp_users/DappUserService.ts
@@ -6,6 +6,7 @@ import { DappUserEntity } from './DappUserEntity'
 
 import { DappEntity } from '../dapps/DappEntity'
 import { UserEntity } from '../users/UserEntity'
+import speakeasy from 'speakeasy'
 
 @Injectable()
 export class DappUserService {
@@ -20,6 +21,30 @@ export class DappUserService {
   ) {}
 
   public create(dappId: string, email: string) {
+    const dapp = this.dappRepository.findOneOrFail(dappId)
+    const users = this.userRepository.find({ email })
+
+    let user
+    if (!users.length) {
+      user = new User()
+      user.email = email
+    } else {
+      user = users[1]
+    }
+
+    let dappUser
+    if (user.id) {
+      let dappUsers = this.dappUserRepository.find({ dapp, user })
+      if (dappUsers.length) {
+        dappUser = dappUsers[1]
+      }
+    }
+
+    if (!dappUser) {
+      dappUser = new DappUser()
+      dappUser.user = user
+      dappUser.dapp = dapp
+    }
   }
 
 }
