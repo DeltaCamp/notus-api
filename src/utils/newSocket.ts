9bd6f634e131d223606c5d71d11f2b5377bcda2c
Brendan Asselstine <mail.asselstine@gmail.com>
2019-03-27 17:35:28 -0700

    Renamed environment variable

--- src/utils/newSocket.ts
+++ src/utils/newSocket.ts
@@ -1,7 +1,7 @@
 const io = require('socket.io-client')
 
 export function newSocket() {
-  const uri = `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`.replace('http', 'ws')
+  const uri = `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.API_PORT}`.replace('http', 'ws')
   console.log(`Connecting to ${uri}`)
   return io(uri)
 }
