326abc4797ba5d48ed11891e4616b7c6a6a7ac32
Brendan Asselstine <mail.asselstine@gmail.com>
2019-03-28 12:13:22 -0700

    Altered client to be websocket only

--- src/utils/newSocket.ts
+++ src/utils/newSocket.ts
@@ -3,5 +3,7 @@ const io = require('socket.io-client')
 export function newSocket() {
   const uri = process.env.NOTUS_API_WS_URI
   console.log(`Connecting to ${uri}`)
-  return io(uri)
+  return io(uri, {
+    transports: ['websocket']
+  })
 }
