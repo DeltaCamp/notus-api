18bec16339a16a1af0ff6dd695ff712e691323ed
Brendan Asselstine <mail.asselstine@gmail.com>
2019-03-27 17:31:09 -0700

    Added console log

--- src/utils/newSocket.ts
+++ src/utils/newSocket.ts
@@ -2,5 +2,6 @@ const io = require('socket.io-client')
 
 export function newSocket() {
   const uri = `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`.replace('http', 'ws')
+  console.log(`Connecting to ${uri}`)
   return io(uri)
 }
