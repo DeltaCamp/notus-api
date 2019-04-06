3da22b548aa67556caa6c95db2b99d4f30bb4e70
Charles Bergeron <chuck.bergeron@gmail.com>
2019-04-05 17:03:14 -0700

    * fix faker data

--- src/engine/BlockListener.ts
+++ src/engine/BlockListener.ts
@@ -31,12 +31,14 @@ export class BlockListener {
     debug(`Received block number ${blockNumber}`)
     transactionContextRunner(async () => {
       const events = await this.eventService.findAllForMatch()
-      await this.checkBlockNumber(events, blockNumber - parseInt(process.env.BLOCK_CONFIRMATION_LEVEL))
+      debug(events)
+      await this.checkBlockNumber(events, blockNumber - parseInt(process.env.BLOCK_CONFIRMATION_LEVEL, 10))
     })
   }
 
   checkBlockNumber = async (events: EventEntity[], blockNumber) => {
     const block: Block = await this.provider.getBlock(blockNumber)
+    debug(block)
     await Promise.all(block.transactions.map(transactionHash => (
       this.handleTransaction(events, block, transactionHash)
     )))
