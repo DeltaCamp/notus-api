39da7a8d930a3af4c256b212ce392ac82ac9354a
Charles Bergeron <chuck.bergeron@gmail.com>
2019-05-28 23:05:28 -0700

    * stash

--- src/common/middleware/LoggerMiddleware.ts
+++ src/common/middleware/LoggerMiddleware.ts
@@ -1,10 +1,47 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+// interface Assignment {
+//   name?: string;
+// }
+
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
-    console.log('Request...');
+    const oldWrite = res.write;
+    const oldEnd = res.end;
+
+    const chunks = [];
+
+    // res.write = (...restArgs) => {
+      // chunks.push(Buffer.from(restArgs[0]));
+      // oldWrite.apply(res, restArgs);
+    // };
+
+    // res.end = (...restArgs) => {
+    //   if (restArgs[0]) {
+    //     chunks.push(Buffer.from(restArgs[0]));
+    //   }
+    //   const body = Buffer.concat(chunks).toString('utf8');
+
+    //   console.log({
+    //     time: new Date().toUTCString(),
+    //     fromIP: req.headers['x-forwarded-for'] ||
+    //       req.connection.remoteAddress,
+    //     method: req.method,
+    //     originalUri: req.originalUrl,
+    //     uri: req.url,
+    //     requestData: req.body,
+    //     responseData: body,
+    //     referer: req.headers.referer || '',
+    //     ua: req.headers['user-agent']
+    //   });
+
+    //   // console.log(body);
+    //   oldEnd.apply(res, restArgs);
+    // };
+
+
     next();
   }
 }
