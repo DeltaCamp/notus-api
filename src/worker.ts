c40a161109ba313454accc1b64b11b6447d24489
Brendan Asselstine <mail.asselstine@gmail.com>
2019-06-11 16:29:55 -0700

    Deleted the WorkerModule

--- src/worker.ts
+++ src/worker.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { WorkerModule } from './WorkerModule';
+import { ApplicationModule } from './ApplicationModule';
 import { BlockListenerManager } from './engine'
 import { rollbar } from './rollbar';
 import { JobRunnerManager } from './jobs/JobRunnerManager';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(WorkerModule);
+  const app = await NestFactory.createApplicationContext(ApplicationModule);
   const jobRunnerManager = app.get(JobRunnerManager)
   await jobRunnerManager.start()
   const blockListenerManager = app.get(BlockListenerManager)
